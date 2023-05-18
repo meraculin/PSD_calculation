e8907431d0479d01edfa2593a34425f1e09a0194
meraculin <syamilmuharror@gmail.com>
2023-05-18 20:01:13 +0900

    updated may 18th

---
+++ .history/spectrogram_20230430155638.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+# Seed the random number generator
+np.random.seed(0)
+
+time_step = .01
+time_vec = np.arange(0, 70, time_step)
+
+# A signal with a small frequency chirp
+sig = np.sin(0.5 * np.pi * time_vec * (1 + .1 * time_vec))
+
+plt.figure(figsize=(8, 5))
+plt.plot(time_vec, sig)
---
+++ .history/spectrogram_20230430155812.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Seed the random number generator
+np.random.seed(0)
+
+time_step = .01
+time_vec = np.arange(0, 70, time_step)
+
+# A signal with a small frequency chirp
+sig = np.sin(0.5 * np.pi * time_vec * (1 + .1 * time_vec))
+
+plt.figure(figsize=(8, 5))
+plt.plot(time_vec, sig)
+plt.show()
